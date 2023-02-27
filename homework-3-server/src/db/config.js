f9619ccabfe0d683f07d0897408df4152b8b92ef
Lnola <nola.luka@gmail.com>
2023-02-27 11:10:23 +0100

    Update default pg port :wrench:

--- homework-3-server/src/db/config.js
+++ homework-3-server/src/db/config.js
@@ -3,6 +3,6 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT || 8765,
+  port: process.env.POSTGRES_PORT || 5432,
   dialect: 'postgres',
 };
