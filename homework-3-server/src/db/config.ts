d24614473415c0c429358ca681c6a79a00abe6e3
Lnola <nola.luka@gmail.com>
2023-02-27 10:03:11 +0100

    Update config.json to config.js 🔧

---
+++ homework-3-server/src/db/config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT || 8765,
+  dialect: 'postgres',
+};
